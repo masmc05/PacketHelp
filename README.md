77e6eb4bbcc16dd6c8faa46a4f105ada6ed0db8f
mastermc05 <masmc05@gmail.com>
2023-01-11 08:34:36 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# This plugin helps with the player info packet bug
+
+***This is not a fix, just make the effect less critical***
+
+we still need to find whi player's data is corrupted
---
+++ src/main/java/me/masmc05/packetlogger/PacketLogger.java
@@ -0,0 +1,82 @@
+package me.masmc05.packetlogger;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mojang.authlib.properties.PropertyMap;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import io.netty.handler.codec.DecoderException;
+import io.papermc.paper.network.ChannelInitializeListenerHolder;
+import net.kyori.adventure.text.Component;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.RemoteChatSession;
+import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+import static net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket.Action.INITIALIZE_CHAT;
+
+public final class PacketLogger extends JavaPlugin {
+    private final long offset;
+    private final Unsafe unsafe;
+
+    public PacketLogger() throws ReflectiveOperationException {
+        Field ffield = null;
+        for (var field : ClientboundPlayerInfoUpdatePacket.class.getDeclaredFields()) {
+            if (field.getType() == List.class) {
+                ffield = field;
+                field.setAccessible(true);
+            }
+        }
+        var unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+        unsafeField.setAccessible(true);
+        unsafe = (Unsafe) unsafeField.get(null);
+        offset = unsafe.objectFieldOffset(Objects.requireNonNull(ffield));
+    }
+    private void afterInitChannel(@NonNull Channel c) {
+        c.pipeline().addBefore("packet_handler", "info_debugger_bbb", new Listener());
+    }
+
+    @Override
+    public void onEnable() {
+        ChannelInitializeListenerHolder.addListener(new NamespacedKey(this, "listen"), this::afterInitChannel);
+    }
+    private final class Listener extends ChannelDuplexHandler {
+        @Override
+        public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
+            if (msg instanceof ClientboundPlayerInfoUpdatePacket packet && packet.actions().contains(INITIALIZE_CHAT)) {
+                var buf = new FriendlyByteBuf(UnpooledByteBufAllocator.DEFAULT.directBuffer());
+                var players = new LinkedList<ClientboundPlayerInfoUpdatePacket.Entry>();
+                for (var entry : packet.entries()){
+                    try {
+                        buf.writeNullable(entry.chatSession(), RemoteChatSession.Data::write);
+                        buf.readNullable(RemoteChatSession.Data::read);
+                        players.add(entry);
+                    } catch (DecoderException e) {
+                        Bukkit.getScheduler().runTask(PacketLogger.this, () -> {
+                            Player player = Bukkit.getPlayer(entry.profile().getId());
+                            if (player == null) return;
+                            player.kick(Component.text("Bad data!"));
+                        });
+                    }
+                }
+                if (players.size() != packet.entries().size()) {
+                    unsafe.putObject(packet, offset, players);
+                }
+            }
+            ctx.write(msg, promise);
+        }
+    }
+}
